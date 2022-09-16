1131996a6871fc7f45e165a14278125e48c82ceb
Lal0gg <2993815280101@ingenieria.usac.edu.gt>
2022-09-15 22:46:34 -0600

    terminando gui

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from ast import Sub
-from tkinter import *
 import tkinter
-from tkinter import font
+from  tkinter import *
+from tkinter import BOTTOM, Menu, Scrollbar, Tk, XView, font
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import scrolledtext
 
 
-#Variables Ventanas
-wnd_menu = None
+
+#Variables globales
+wnd_menu = None #ventana menu
+filepath = "" #contenedor del archivo a analizar
+
 
 def window_mainMenu():
     global wndw_menu
@@ -23,9 +28,9 @@ def window_mainMenu():
     #Items del menú1
     file_menu = Menu(SubMenu,tearoff=0)
     SubMenu.add_cascade(label="Archivo",menu=file_menu)
-    file_menu.add_command(label="Abrir",command=our_command,font=("Courier 10 bold"),background="#E4E3FF")
+    file_menu.add_command(label="Abrir",command=openfile,font=("Courier 10 bold"),background="#E4E3FF")
     file_menu.add_separator()
-    file_menu.add_command(label="Guardar",command=our_command,font=("Courier 10 bold"),background="#E4E3FF")
+    file_menu.add_command(label="Guardar",command=savefile,font=("Courier 10 bold"),background="#E4E3FF")
     file_menu.add_separator()
     file_menu.add_command(label="Guardar Como",command=our_command,font=("Courier 10 bold"),background="#E4E3FF")
     file_menu.add_separator()
@@ -41,11 +46,46 @@ def window_mainMenu():
     help_menu.add_separator()
     help_menu.add_command(label="Manual Técnico",command=our_command,font=("Courier 10 bold"),background="#E4E3FF")
     help_menu.add_separator()
-    help_menu.add_command(label="Temas de Ayuda",command=our_command,font=("Courier 10 bold"),background="#E4E3FF")
+    help_menu.add_command(label="Temas de Ayuda",command=datau,font=("Courier 10 bold"),background="#E4E3FF")
+    
+    scroll1 = scrolledtext.ScrolledText(wndw_menu, width =60, height=40,font=('Courier', 10),wrap=WORD)
+    scroll1.place(x=0,y=10)
+    scroll1.focus()
+
+    scroll2 = scrolledtext.ScrolledText(wndw_menu, width =60, height=40, font=('Courier', 10),wrap = WORD)
+    scroll2.place(x=515,y=10)
+    scroll2.focus()
 
     wndw_menu.mainloop()
 
 def our_command():
     print("Hola Mundo")
+
+def datau():
+    mensaje=messagebox.showinfo("Datos"," Curso: Lenguajes Formales y de Programación\n Nombre: Eduardo Josué González Cifuentes\n Carnet: 201900647 \n Sección: A-")
+
+
+def openfile():
+    global filepath
+    try:
+        filepath = filedialog.askopenfilename()
+        file = open(filepath,'r+',encoding="utf-8")
+        print(file.read())
+        file.close()
+    except:
+        pass
+
+def savefile():
+    global filepath
+    try: 
+        file = open(filepath,'w',encoding="utf-8")
+        file.write("hola buenas :v")
+        file.close()
+    except:
+        pass
+
+def scanner():
+    pass
+
 
 window_mainMenu()
